8e3e7c7cec93a7e442411174f33d0c42ca395ed4
etienne.toupin <etienne.toupin@usherbrooke.ca>
2026-01-25 12:42:34 -0500

    Initial commit

---
+++ src/main/java/ca/pandaaa/animalQuest/AnimalQuest.java
@@ -0,0 +1,17 @@
+package ca.pandaaa.animalQuest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnimalQuest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // System d'exp
+        // System de level
+        // System de mana (avec la XP bar)
+        // System d'items
+        // System de menus / shops
+        // System d'aptitudes (vie, strength,
+        // System de job
+    }
+}
